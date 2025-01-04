9a5e0f96b0d970bd58d4bd0681ff023998293591
paracetamol <140186933+apoo3301@users.noreply.github.com>
2025-01-04 19:22:14 +0100

    add dock

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,56 @@
-import Image from "next/image";
+"use client";
+
+import { IconBrandGithub, IconBrandX, IconExchange, IconHome, IconNewSection, IconTerminal2 } from "@tabler/icons-react";
+import { FloatingDock } from "@/components/ui/floating-dock";
 
 export default function Home() {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Products",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Components",
+      icon: (
+        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Changelog",
+      icon: (
+        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
+
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
+    <div className="min-h-screen bg-black flex items-center justify-center relative">
       <main className="relative w-full max-w-[800px] aspect-video">
         <video
           autoPlay
@@ -15,6 +63,10 @@ export default function Home() {
           Votre navigateur ne supporte pas la lecture vidéo.
         </video>
       </main>
+      
+      <div className="fixed bottom-8 left-1/2 -translate-x-1/2">
+        <FloatingDock items={links} />
+      </div>
     </div>
   );
 }
